05b3cf8b9844eb793b1e4c8a6df6dedb79c0b653
Marco De Crema <marcodecrema@libero.it>
2025-11-02 19:31:37 +0100

    requested fix - 1

--- src/app/pages/solution-details/solution-details.component.ts
+++ src/app/pages/solution-details/solution-details.component.ts
@@ -31,45 +31,52 @@ export class SolutionDetailsComponent  implements OnInit {
     this.solutionList = [
       {
         id: 0,
-        name: 'Paint',
+        name: 'LoLiPaint',
         mainDescription: 'Biopolimeri alternativi alle tradizionali resine sintetiche come acrilati, siliconi, poliuretani',
-        description: 'Queste matrici polimeriche offrono la possibilità di formulare pitture e vernici completamente naturali, prive di VOC e ritardanti di fiamma. Sono particolarmente adatte ad essere applicate nel settore delle pitture per l’infanzia,  per l’uso sulla pelle e decorative indoor, essendo prive di sostanze tossiche, anzi addirittura edibili e aventi proprietà cosmetiche dermocompatibili.'
+        description: 'Queste matrici polimeriche offrono la possibilità di formulare pitture e vernici completamente naturali, prive di VOC e ritardanti di fiamma. Sono particolarmente adatte ad essere applicate nel settore delle pitture per l’infanzia,  per l’uso sulla pelle e decorative indoor, essendo prive di sostanze tossiche, anzi addirittura edibili e aventi proprietà cosmetiche dermocompatibili.',
+        image: 'pittura-1.jpeg'
       },
       {
         id: 1,
-        name: 'Phyto',
+        name: 'LoLiPhyto',
         mainDescription: 'Biopolimeri adatti ad essere dispensati su foglie e frutti delle piante nel settore agricolo',
-        description: 'Perfetti per l’agricoltura biologica, questi polimeri vengono formulati per creare dei gel adatti ad inglobare additivi e sostanze anticrittogamiche, erbicidi e insetticidi. Spesso queste sostanze sono instabili e una volta che sono applicati sulla vegetazione, vengono facilmente degradati e dilavati dagli agenti atmosferici. Questi prodotti permettono un maggior tempo di permanenza, una maggiore esposizione ed un lento rilascio che garantisce una minore frequenza di applicazione e minori sprechi.'
+        description: 'Perfetti per l’agricoltura biologica, questi polimeri vengono formulati per creare dei gel adatti ad inglobare additivi e sostanze anticrittogamiche, erbicidi e insetticidi. Spesso queste sostanze sono instabili e una volta che sono applicati sulla vegetazione, vengono facilmente degradati e dilavati dagli agenti atmosferici. Questi prodotti permettono un maggior tempo di permanenza, una maggiore esposizione ed un lento rilascio che garantisce una minore frequenza di applicazione e minori sprechi.',
+        image: 'idrogeli-1.avif'
       },
       {
         id: 2,
-        name: 'Compo',
+        name: 'LoLiCompo',
         mainDescription: 'Biopolimeri utili come alternativa ai tradizionali polimeri leganti di cariche inerti in materiali compositi',
-        description: 'Scopri le proprietà delle nuove matrici polimeriche che possono offrire una valida soluzione nel creare materiali compositi resistenti, senza VOC e leggeri. La speciale matrice offre inoltre ottime proprietà ritardanti di fiamma, trasparenza e possibilità di cross-linking modulare. Se si vuole concepire nuovi compositi adatti ad essere facilmente smaltiti o addirittura biodegradabili, LoLicompo potrebbe essere la soluzione giusta!'
+        description: 'Scopri le proprietà delle nuove matrici polimeriche che possono offrire una valida soluzione nel creare materiali compositi resistenti, senza VOC e leggeri. La speciale matrice offre inoltre ottime proprietà ritardanti di fiamma, trasparenza e possibilità di cross-linking modulare. Se si vuole concepire nuovi compositi adatti ad essere facilmente smaltiti o addirittura biodegradabili, LoLicompo potrebbe essere la soluzione giusta!',
+        image: 'img-1.jpg'
       },
       {
         id: 3,
-        name: 'Pack',
+        name: 'LoLiPack',
         mainDescription: 'Biopolimeri per una nuova generazione di packaging plastic-free',
-        description: 'Molte Aziende nel mondo stanno sviluppando film sottili, contenitori rigidi, finestre trasparenti con questi nuovi materiali. Abbiamo collaborato con diverse di queste Aziende, leader nel settore, per aiutarle a sviluppare valide alternative ai prodotti in commercio.'
+        description: 'Molte Aziende nel mondo stanno sviluppando film sottili, contenitori rigidi, finestre trasparenti con questi nuovi materiali. Abbiamo collaborato con diverse di queste Aziende, leader nel settore, per aiutarle a sviluppare valide alternative ai prodotti in commercio.',
+        image: 'cosmetic-3.jpg'
       },
       {
         id: 4,
-        name: 'Layer',
+        name: 'LoLiLayer',
         mainDescription: 'Biopolimeri che creano film sottili adatti ad essere poliaccoppiati ad altri materiali',
-        description: 'Una valida alternativa alla plastica conferendo elasticità al materiale. Adatti anche a produrre membrane porose particolarmente utili ad essere applicate nell’industria tessile come strati piu’ o meno sottili aventi proprietà isolanti e traspiranti.'
+        description: 'Una valida alternativa alla plastica conferendo elasticità al materiale. Adatti anche a produrre membrane porose particolarmente utili ad essere applicate nell’industria tessile come strati piu’ o meno sottili aventi proprietà isolanti e traspiranti.',
+        image: 'sheet-mask-2.avif'
       },
       {
         id: 5,
-        name: 'Beauty',
+        name: 'LoLiBeauty',
         mainDescription: 'Biopolimeri che creano film sottili adatti ad essere poliaccoppiati ad altri materiali',
-        description: 'Una valida alternativa alla plastica conferendo elasticità al materiale. Adatti anche a produrre membrane porose particolarmente utili ad essere applicate nell’industria tessile come strati piu’ o meno sottili aventi proprietà isolanti e traspiranti.'
+        description: 'Una valida alternativa alla plastica conferendo elasticità al materiale. Adatti anche a produrre membrane porose particolarmente utili ad essere applicate nell’industria tessile come strati piu’ o meno sottili aventi proprietà isolanti e traspiranti.',
+        image: 'cosmetic-1.jpeg'
       },
       {
         id: 6,
-        name: 'Idea',
+        name: 'LoLiIdea',
         mainDescription: 'Proponici una tua idea',
-        description: 'Se hai un’idea che pensi possa essere interessante per lo sviluppo di un nuovo prodotto aziendale o per migliorare quelli già esistenti, rivolgiti a noi! Siamo a tua disposizione per valutare le opzioni possibili e per supportarti nello sviluppo del prodotto piu’ adatto alle tue esigenze.'
+        description: 'Se hai un’idea che pensi possa essere interessante per  lo sviluppo di un nuovo prodotto aziendale o per migliorare quelli già esistenti, rivolgiti a noi! Siamo a tua disposizione per valutare le opzioni possibili e per supportarti nello sviluppo del prodotto piu’ adatto alle tue esigenze.',
+        image: 'bg-15.png'
       }
     ];
 
